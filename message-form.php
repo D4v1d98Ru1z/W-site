b7597f2350e6562d9aff87d278fdcbfe3e8e6404
David Ruiz <davidl.ruizc@gmail.com>
2018-03-21 23:50:12 -0500

    php form adding

---
+++ message-form.php
@@ -0,0 +1,19 @@
+<?php
+    $name = $_POST['Name'];
+    $Email_user = $_POST['Email'];
+    $message = $_POST['Message'];
+
+    // DavidPortfolio@davidruiz.com
+    $email_from = 'davidl-ruizc@outlook.com';
+    $email_subject = "New message from portfolio";
+    $email_body = "Name: $name.\n".
+                    "Email: $Email_user.\n".
+                    "Message: $message.\n";
+    
+    $to = "davidl-ruizc@outlook.com";
+    $headers = "From: $email_from\r\n";
+    $headers .= "Reply-To: $Email_user\r\n";
+
+    mail($to, $email_subject, $email_body, $headers);
+    header("Location: index.html");
+?>
