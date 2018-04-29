ed61266c30ff85f6edb8cf0d0f06a058b0bb6089
David <davidl.ruizc@gmail.com>
2018-04-28 23:14:37 -0500

    Change Email Style

--- PHP/Enviar.php
+++ PHP/Enviar.php
@@ -15,14 +15,14 @@
 				$mail->Host = '	mx1.hostinger.co';  				  // Specify main and backup SMTP servers
 				$mail->SMTPAuth = true;                               // Enable SMTP authentication
 				$mail->Username = '---Email----';                 // SMTP username
-				$mail->Password = '-----Password------';                           // SMTP password
+				$mail->Password = '---Password----';                           // SMTP password
 				$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 				$mail->Port = 587;                                    // TCP port to connect to
 				$mail->CharSet = 'UTF-8';
 				//Recipients
-				$mail->setFrom('support@sgc.andreslargo.com', 'Sistema SGC');
+				$mail->setFrom('support@sgc.andreslargo.com', 'Portfolio Site');
 				//$mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-				$email = "---destination email-----";
+				$email = "----destination email-----";
 				$mail->addAddress($email);               // Name is optional
 		
 				//$mail->addReplyTo('info@example.com', 'Information');
@@ -33,26 +33,31 @@
 				//	$mail->AddEmbeddedImage('../img/sgc.png', 'logo_2u');
 				//Content
 				$mensaje = $_POST['Message'];
+				$destinatario = $_POST['Email'];
+				$nombrepro = $_POST['Name'];
 				$mail->isHTML(true);                                  // Set email format to HTML
-				$mail->Subject = '¿FUNCIONARÁ?';
+				$mail->Subject = 'New Portfolio Message';
 				$mail->Body = " 
 					<html>
-					<body style= \"background: #000; color: #fff; padding: 19px; text-align: center;\">
+					<body style= \"background: #fff; color: #B1B1B1; padding: 19px; text-align: center;\">
 						<header style =\"font-family:\'Sans Serif\';\">
-							<h1 style= \" color: #ffffff; font-size:22px;\">
-								<strong style= \"color: #ffffff;\">¡Enhorabuena!</strong> Hola <br><br>
-							Esta es una prueba exitosa :v</h1>
+							<h1 style= \" color: #B1B1B1; font-size:18px;\">
+								<strong style= \"color: #B1B1B1;\">YOU HAVE A NEW MESSAGE!</strong><br><br>
+							</h1>
 						</header>
-						<main style= \"color: #fff; font-size: 22px;\">
-							<h3 style= \"color: #fff; font-size: 22px;\">
-								Al iniciar sesión te pediremos que cambies tu contraseña por seguridad.
+						<main style= \"color: #484848; font-size: 18px;\">
+							<h3 style= \"color: #484848; font-size: 18px;\">
 								<br>
-								SU MENSAJE: $mensaje
+								$nombrepro te saluda
+								<br>
+								El Mensaje fue enviado por: $destinatario								 
+								<br>
+								Mensaje: $mensaje
+
 								</h3>
 						</main>
 						<br><br>
-						<a style = \"font-size: 22px; background: black; color: white; border: 1px solid white; border-radius: 5px; padding: 10px; text-decoration: none; padding-right: 16px; padding-left: 16px; \" href = \"http://www.andreslargo.com/sgc/login.html\"> Inicia sesión </a>
-						<p style = \"font-size: 22px; color: #fff;\">Cordialmente, Andrés Largo.</p>
+						<a style = \"font-size: 16px; background: #77BE53; color: white; border: 1px solid white; border-radius: 2px; padding: 10px; text-decoration: none; padding-right: 16px; padding-left: 16px; \" href = \"https://github.com/D4v1d98Ru1z/W-site\"> Lovely </a>
 					</body>
 					</html>";
 				$mail->AltBody = '¿Esto está bien?';
