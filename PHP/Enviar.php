3cd35c8f2ccdf6250676535f951e8a876c9e5796
David Ruiz <davidl.ruizc@gmail.com>
2018-05-09 01:11:40 -0500

    mail with smtp gmail

--- PHP/Enviar.php
+++ PHP/Enviar.php
@@ -12,12 +12,24 @@
 				//Server settings
 				$mail->SMTPDebug = 2;                                 // Enable verbose debug output
 				$mail->isSMTP();                                      // Set mailer to use SMTP
-				$mail->Host = '	mx1.hostinger.co';  				  // Specify main and backup SMTP servers
+				$mail->Host = '	mx1.hostinger.co';
+				//$mail->Host = 'smtp.gmail.com';   				  // Specify main and backup SMTP servers
 				$mail->SMTPAuth = true;                               // Enable SMTP authentication
 				$mail->Username = '---Email----';                 // SMTP username
 				$mail->Password = '---Password----';                           // SMTP password
+				
 				$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+				
 				$mail->Port = 587;                                    // TCP port to connect to
+				//Para envio por Gmail                              // TCP port to connect to				
+				// $mail->SMTPOptions = array(
+				// 	'ssl' => array(
+				// 		'verify_peer' => false,
+				// 		'verify_peer_name' => false,
+				// 		'allow_self_signed' => true
+				// 	)
+				// );
+				
 				$mail->CharSet = 'UTF-8';
 				//Recipients
 				$mail->setFrom('support@sgc.andreslargo.com', 'Portfolio Site');
